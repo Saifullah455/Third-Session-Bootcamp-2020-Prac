600f554d00287ee329fe0375ab0e0819c4f46e6f
Saifullah455 <41240651+Saifullah455@users.noreply.github.com>
2020-08-10 17:25:38 +0500

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+// import logo from './logo.svg';
 import './App.css';
 // import SAIF from './test1';
 import SemCourses from './semCourses';
@@ -16,6 +17,8 @@ function App() {
       <h1>Department of Computer Science</h1>
       <SemCourses semNum="5th" course1="NDC" course2="AI" course3="CC" course4="OB" course5="NA" course6="CA" />
       <GPA gpa="2.54"/>
+      <SemCourses semNum="4th" course1="ODE" course2="AL" course3="BE" course4="ISE" course5="ICS" course6="OOP" />
+      <GPA gpa="3.24"/>
     </div>
 
   );
